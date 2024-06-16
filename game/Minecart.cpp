ccf543341e536e6ce38ffaf2dd33698c512c3f47
Henrik Tietjens <henrik.tietjens@stud-mail.uni-wuerzburg.de>
2024-06-16 15:27:35 +0200

    quirin = opfer

---
+++ game/Minecart.cpp
@@ -0,0 +1,6 @@
+#include "Minecart.h"
+
+void Minecart::update()
+{
+
+}
---
+++ game/Minecart.h
@@ -0,0 +1,12 @@
+#include <glm/glm.hpp>
+
+class Minecart {
+public:
+
+	void update();
+
+private:
+	
+	glm::vec2 position;
+
+};
