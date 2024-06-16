fce7a3407265427fa9a7c2591deb4816b7abfb60
Qu0d0 <130589729+Qu0d0@users.noreply.github.com>
2024-06-16 20:15:29 +0200

    Spline

---
+++ game/Spline.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "Spline.h"
+
+Spline::Spline()
+{
+
+}
+
+float Spline::sampleHight(float x)
+{
+	return 0.0f;
+}
+
+void Spline::addNextPoint(glm::vec2 p)
+{
+	splinePoints.push_back(p);
+}
+
+glm::vec2 Spline::getPoint(float t)
+{
+	return glm::vec2();
+}
+
+glm::vec2 Spline::getPoint(float t, int n)
+{
+	float x = 0;
+	float y = 0;
+	glm::vec2 p1 = splinePoints[n];
+	glm::vec2 p2 = splinePoints[n + 1];
+	glm::vec2 v1 = splinePoints[n + 1] - splinePoints[n - 1];
+	glm::vec2 v2 = splinePoints[n + 2] - splinePoints[n];
+
+	float t2 = t * t;
+	float t3 = t2 * t;
+
+	float h00 = 2 * t3 - 3 * t2 + 1;
+	float h10 = t3 - 2 * t2 + t;
+	float h01 = -2 * t3 + 3 * t2;
+	float h11 = t3 - t2;
+
+	x = h00 * p1.x + h10 * v1.x + h01 * p2.x + h11 * v2.x;
+	y = h00 * p1.y + h10 * v1.y + h01 * p2.y + h11 * v2.y;
+
+	return glm::vec2(x,y);
+}
---
+++ game/Spline.h
@@ -0,0 +1,18 @@
+#include <list>
+#include <glm/glm.hpp>
+#include <vector>
+
+class Spline {
+
+public:
+	Spline();
+
+	float sampleHight(float x);
+	void addNextPoint(glm::vec2 p);
+	glm::vec2 getPoint(float t);
+	glm::vec2 getPoint(float t, int n);
+
+	std::vector<glm::vec2> splinePoints;
+
+
+};
