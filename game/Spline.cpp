33c909561821ef53529d9144422ecb6cd2a91845
Qu0d0 <130589729+Qu0d0@users.noreply.github.com>
2024-06-17 14:08:14 +0200

    Spline tangent and normal function

--- game/Spline.cpp
+++ game/Spline.cpp
@@ -63,3 +63,39 @@ glm::vec2 Spline::getPoint(float t, int n)
 
 	return glm::vec2(x,y);
 }
+
+glm::vec2 Spline::getTangent(float t, int n)
+{
+	float x = 0;
+	float y = 0;
+	glm::vec2 p1 = splinePoints[n];
+	glm::vec2 p2 = splinePoints[n + 1];
+	glm::vec2 v1 = splinePoints[n + 1] - splinePoints[n - 1];
+	glm::vec2 v2 = splinePoints[n + 2] - splinePoints[n];
+
+	float t2 = t * t;
+	float t3 = t2 * t; 
+
+	float h00 = 6 * t2 - 6 * t;
+	float h10 = 3 * t2 - 4 * t + 1;
+	float h01 = -6 * t2 + 6 * t;
+	float h11 = 3 * t2 - 2 * t;
+
+	x = h00 * p1.x + h10 * v1.x + h01 * p2.x + h11 * v2.x;
+	y = h00 * p1.y + h10 * v1.y + h01 * p2.y + h11 * v2.y;
+
+	glm::vec2 tangent = glm::vec2(x, y);
+
+	tangent = glm::normalize(tangent);
+
+	return tangent;
+}
+
+glm::vec2 Spline::getNormal(float t, int n)
+{
+	glm::vec2 normal = getTangent(t, n);
+	float temp = normal.x;
+	normal.x = normal.y;
+	normal.y = -temp;
+	return normal;
+}
--- game/Spline.h
+++ game/Spline.h
@@ -11,8 +11,8 @@ public:
 	void addNextPoint(glm::vec2 p);
 	glm::vec2 getPoint(float t);
 	glm::vec2 getPoint(float t, int n);
+	glm::vec2 getTangent(float t, int n);
+	glm::vec2 getNormal(float t, int n);
 
 	std::vector<glm::vec2> splinePoints;
-
-
 };
